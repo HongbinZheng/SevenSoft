893e1c6c2de520d1b1026f3d7c77bff6f2845186
HongbinZheng <36983590+HongbinZheng@users.noreply.github.com>
2018-10-18 15:31:14 -0700

    Update profile.js

--- client/src/components/User/profile.js
+++ client/src/components/User/profile.js
@@ -5,6 +5,7 @@ class Profile extends Component {
     constructor(){
         super()
         this.Auth = new Authserver()
+        this.handleLogout.bind(this)
     }
     componentWillMount(){
         if(!this.Auth.loggedIn()){
@@ -12,10 +13,19 @@ class Profile extends Component {
         }
     }
 
+    handleLogout(e){
+        e.preventDefault();
+        this.Auth.logout();
+        this.props.history.replace('/')
+    }
+
     render() {
         return (
             <div>
                 This is Profile page
+                <p className="App-intro">
+            <button type="button" className="form-submit" onClick={this.handleLogout.bind(this)}>Logout</button>
+                 </p>
             </div>
         );
     }
