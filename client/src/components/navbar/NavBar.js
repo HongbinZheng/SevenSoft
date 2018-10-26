2cec15a1be60fca56d389e0196fbbea2c4f9bab6
Bryan Gallardo <bryanjgallardo@gmail.com>
2018-10-25 18:26:46 -0700

    Fixed bootstrap and changed navbar to default version.

--- client/src/components/navbar/NavBar.js
+++ client/src/components/navbar/NavBar.js
@@ -1,39 +1,44 @@
 import React from 'react';
 import {Link } from 'react-router-dom';
-import { Navbar,Nav,NavItem } from 'react-bootstrap';
 
 
 const NavBar = () => {
         return (
-         <div>
-            <Navbar inverse collapseOnSelect>
-  <Navbar.Header>
-    <Navbar.Brand>
-     <Link to='/'>SevenSoft</Link>
-    </Navbar.Brand>
-    <Navbar.Toggle />
-  </Navbar.Header>
-  <Navbar.Collapse>
-    <Nav>
-      <NavItem eventKey={1} href={`/${'produce'}`}>
-        Produce
-      </NavItem>
-      <NavItem eventKey={2} href={`/${'Meats'}`}>
-        Meats
-      </NavItem>
-      <NavItem eventKey={3} href={`/${'Dairy'}`}>
-        Dairy
-      </NavItem>
-      <NavItem eventKey={4} href={`/${'Beverages'}`}>
-        Beverages
-      </NavItem>
-      <NavItem eventKey={5} href="/profile">
-        User
-      </NavItem>
-    </Nav>
-  </Navbar.Collapse>
-</Navbar>;
-         </div>
+             <nav className="navbar sticky-top navbar-expand-lg navbar-light bg-light">
+                 <a className="navbar-brand" href="/">Navbar</a>
+                 <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                     <span className="navbar-toggler-icon"></span>
+                 </button>
+
+                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                     <ul className="navbar-nav mr-auto">
+                         <li className="nav-item active">
+                             <a className="nav-link" href="/">Home <span className="sr-only">(current)</span></a>
+                         </li>
+                         <li className="nav-item">
+                             <a className="nav-link" href="/">Link</a>
+                         </li>
+                         <li className="nav-item dropdown">
+                             <a className="nav-link dropdown-toggle" href="/" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                 Dropdown
+                             </a>
+                             <div className="dropdown-menu" aria-labelledby="navbarDropdown">
+                                 <a className="dropdown-item" href="/">Action</a>
+                                 <a className="dropdown-item" href="/">Another action</a>
+                                 <div className="dropdown-divider"></div>
+                                 <a className="dropdown-item" href="/">Something else here</a>
+                             </div>
+                         </li>
+                         <li className="nav-item">
+                             <a className="nav-link disabled" href="/">Disabled</a>
+                         </li>
+                     </ul>
+                     <form className="form-inline my-2 my-lg-0">
+                         <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
+                         <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+                     </form>
+                 </div>
+             </nav>
                         
         );
     
