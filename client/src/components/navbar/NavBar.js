82d6a8cacde19acc8b465a23f43ceab81f8ac1a5
HongbinZheng <36983590+HongbinZheng@users.noreply.github.com>
2018-10-31 15:56:45 -0700

    add shopping cart

--- client/src/components/navbar/NavBar.js
+++ client/src/components/navbar/NavBar.js
@@ -6,8 +6,8 @@ import style from './navbar.css';
 
 const NavBar = () => {
         return (
-            <div>
-             <nav className="navbar sticky-top navbar-expand-lg navbar-light bg-light center" id = "navBar">
+            <div className="sticky-top">
+             <nav className="navbar  navbar-expand-lg navbar-light bg-light center" id = "navBar">
                  <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                      <span className="navbar-toggler-icon"></span>
                  </button>
