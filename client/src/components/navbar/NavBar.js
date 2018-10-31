6016e692d9af36ff50fba5d0123ee34b56b93ae7
it-jinshan <it.jinshan@gmail.com>
2018-10-31 11:00:33 -0700

    Fixed banner img, fixed navbar

--- client/src/components/navbar/NavBar.js
+++ client/src/components/navbar/NavBar.js
@@ -1,40 +1,30 @@
 import React from 'react';
 import {Link } from 'react-router-dom';
+import style from './navbar.css';
 
 
 const NavBar = () => {
         return (
-             <nav className="navbar sticky-top navbar-expand-lg navbar-light bg-light">
-                 <a className="navbar-brand" href="/">Navbar</a>
+             <nav className="navbar sticky-top navbar-expand-lg navbar-light bg-light center" id = "navBar">
                  <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                      <span className="navbar-toggler-icon"></span>
                  </button>
 
-                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                     <ul className="navbar-nav mr-auto">
-                         <li className="nav-item active">
-                         </li>
-                         <li className="nav-item">
-                             <a className="nav-link" href="/Meats">Meats</a>
-                             <a className="nav-link" href="/Dairy">Dairy</a>
-                             <a className="nav-link" href="/Produce">Produce</a>
-                             <a className="nav-link" href="/Beverages">Beverages</a>
-                             <a className="nav-link" href="/Snacks">Snacks</a>
-                             <a className="nav-link" href="/profile">Profile</a>                   
-                         </li>
-                         <li className="nav-item">
-                             <a className="nav-link disabled" href="/">Disabled</a>
-                         </li>
+                 <div className="collapse navbar-collapse mx-auto" id="navbarSupportedContent">
+                     <ul className="navbar-nav mr-auto btn-group-lg">
+                        <a href="/produce" className ="btn btn-light text-center" role="button" aria-pressed="true">Produce</a>
+                        <a href="/meats" className ="btn btn-light text-center" role="button" aria-pressed="true">Meats</a>
+                        <a href="/beverages" className ="btn btn-light text-center" role="button" aria-pressed="true">Beverages</a>
+                        <a href="/dairy" className ="btn btn-light text-center" role="button" aria-pressed="true">Dairy</a>
+                        <a href="/" className ="btn btn-light text-center" role="button" aria-pressed="true">More>></a>
+                        <a href="/profile" className ="btn btn-light text-center" role="button" aria-pressed="true">Login</a>
                      </ul>
-                     <form className="form-inline my-2 my-lg-0">
-                         <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
-                         <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-                     </form>
                  </div>
-             </nav>
-                        
+             </nav>        
         );
-     }
     
+    
+}
 
-export default NavBar;
+export default NavBar;         
+        
