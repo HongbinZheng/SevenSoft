dbaf18af8da4c89879a23e766ebb57552d1b4df8
HongbinZheng <36983590+HongbinZheng@users.noreply.github.com>
2018-11-08 23:39:56 -0800

    added backend firebase, show most recent orders in home page, make cart disapper when nothing in there

--- client/src/components/shopping cart/Cart.js
+++ client/src/components/shopping cart/Cart.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import CartItem from './CartItem';
 import { withRouter } from "react-router-dom";
-import style from "./Cart.css"
 
 class Cart extends React.Component {
     constructor(props) {
@@ -101,6 +100,7 @@ class Cart extends React.Component {
     
     render() {
         return(
+            this.state.cartItems.length > 0 ?
               <div className = "CartDiv" style = {{position:'absolute', right:'45px', top:'100px'}}>
                 <button className="btn btn-success" role="button" data-toggle="collapse" data-target="#cartopen" aria-expanded="false" aria-controls="#cartopen" aria-haspopup="true" text-align="center" margin = "auto" style={{width:'80px', height:'80px', borderRadius:'50%'}}>
                     <i className="fas fa-shopping-cart fa-2x"> {this.getTotalQyt(this.state.cartItems)}</i>
@@ -122,7 +122,7 @@ class Cart extends React.Component {
                         <button className="fas fa-shopping-cart fa-5x btn btn-info" style={{textAlign:"center",fontSize:30, marginLeft: '50px', marginBottom:'10px', marginTop:'10px'}} onClick={()=>this.handleCheckoutClick(this.state.cartItems)}> CheckOut </button>
                     </div>
             </div>
-            </div>
+            </div> : null
            // </div>
            // </div>
         )
