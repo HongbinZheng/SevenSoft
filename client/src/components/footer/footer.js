5adb2af6690837b2dfd6ad741e75958905f39a3c
lancey <qvbingo@gmail.com>
2018-11-14 12:03:37 -0800

    footer

--- client/src/components/footer/footer.js
+++ client/src/components/footer/footer.js
@@ -1,16 +1,22 @@
-import React from 'react';
-import style from './footer.css'
+import React from "react";
+import style from "./footer.css";
 
 const Footer = () => {
-        return (
-            <div className={style.footer}>
-                <div className={style.info}>
-                    <h1><p>Contact information</p></h1>
-                         <p>No contact info avaliable</p>
-                         <p>TAKE WHAT YOU GET</p>                   
-                </div>
-            </div>
-        );
-}
+  return (
+    <div className={style.footer}>
+      <div className={style.info}>
+        <h2>
+          <p>Contact Us</p>
+        </h2>
+        <a href="https://www.google.com">Terms and Conditions</a>
+        <p>
+          <a href="https://www.safeway.com/ShopStores/Privacy-Policy.page">
+            Privacy Policy
+          </a>
+        </p>
+      </div>
+    </div>
+  );
+};
 
 export default Footer;
---
+++ client/src/components/footer/privacy.js
@@ -0,0 +1,158 @@
+import React from "react";
+import style from "./footer.css";
+
+const Privacy = () => {
+  return (
+    <div>
+      <div>
+        <h1>
+          <p>Privacy Policy</p>
+        </h1>
+        <h2>
+          <p>CONSENT TO PRIVACY POLICY</p>
+        </h2>
+        <p>
+          By accessing our website to obtain information or services; by
+          providing solicited or unsolicited personal information to us online,
+          at any of our retail locations, or through one of our mobile
+          applications (“apps”); by enrolling in one of our customer loyalty or
+          other programs, or through other means, you are consenting to our use
+          of such information, subject to applicable laws, and agreeing to the
+          terms of this Privacy Policy.
+        </p>
+
+        <h2>
+          <p>DEFINITION OF PERSONAL INFORMATION</p>
+        </h2>
+        <p>
+          We define the term “personal information” to mean any information that
+          we directly associate with, or use to identify, a specific person.
+          This may include information such as an address, phone number, email
+          address, or fax number; a user name or other online registration
+          information; a financial account or credit card number, and
+          transaction information or purchase history, when associated with a
+          person’s name. Personal information does not include “aggregated
+          information,” which is information that we collect about a group or
+          category of persons or services. It also does not include
+          “de-identified information,” which is information from which we or our
+          agents have removed any information that can be used to specifically
+          identify a person. This policy does not restrict or limit our
+          collection, use, or disclosure of aggregated or de-identified
+          information.
+        </p>
+
+        <h2>
+          <p>COLLECTION OF INFORMATION</p>
+        </h2>
+        <p>
+          We may collect your personal and other information described below in
+          various ways, including, but not limited to, the following:
+        </p>
+
+        <p>
+          *Personal Information You Provide. We may request, or give you the
+          opportunity to provide, personal information when you register as a
+          user of our website or one of our mobile applications, shop online or
+          in our stores, sign up for our programs, offers, or sweepstakes, take
+          one of our surveys, use our products or services, cash a check,
+          contact us about a prior purchase, or email, call, or otherwise
+          communicate with us. Similarly, if you contact us regarding a product
+          or service that you are interested in or concerned about, we may
+          request that you provide personal information that will enable us to
+          respond to your inquiry.*Automatically Collected Information. When you
+          shop online or in our stores or register for or use any of our online,
+          mobile or other services, transactional and other information, such as
+          the date of your purchase, registration, or service usage, their
+          descriptions and, if applicable, the dollar amount or value of the
+          transaction, your Internet protocol address, browser type, domain
+          names, access times, operating systems, mouse clicks and movements,
+          and scrolling activities may be collected automatically or “passively”
+          by us or by one or more of our third party service providers. For
+          example, we may use third-party analytics tools that collect
+          information about use of our websites and user traffic. We treat this
+          information as personal information if we combine it with or link it
+          to other information that may identify you.*Location Information. If
+          you use one of our mobile applications and your mobile device’s
+          settings allow it, we may collect information about your real-time
+          location. You may adjust the settings on your mobile device to allow
+          or prevent the sharing of location information. For example, you can
+          disable “Location” (or “Location Services” on iOS-based devices) on
+          your mobile device to prevent sharing your location information with
+          us. Please refer to instructions provided by your mobile service
+          provider or the manufacturer of your mobile device to learn how to
+          adjust your mobile device settings. Please note that if you disable
+          the sharing of location information, you may be unable to access some
+          features of our services that are designed for mobile devices.*Mobile
+          Device Information. We may also collect mobile device information
+          using beaconing technology, such as: the device type, model and
+          operating system version, and current IP address. We do not use this
+          technology to collect personal information such as name, mobile
+          telephone number, or email address. To disable our ability to identify
+          your mobile device, you can delete our app, adjust the in-app
+          settings, or turn your Bluetooth to “off” on your device.*Other
+          Information. We may obtain information about you from third parties
+          such as demographic marketing information, address verification, or
+          mailing list information and combine it with other personal
+          information pertaining to you.*Cookies, Web Beacons and Similar
+          Technologies. We may use service providers to host or provide content
+          on some of our websites. We and/or a service provider may use cookies,
+          web beacons, Uniform Resource Locators (URL), and similar technologies
+          to manage our websites and email messages and to collect and track
+          information about you and your activities online at our websites over
+          time and across third-party websites or online services including, but
+          not limited to, your computer’s Internet protocol address and
+          operating system, your browser type, the site from which you linked to
+          our site, the time and date of your visit, how you use our services,
+          and what products and services you purchase. In addition, we or a
+          service provider may use local shared objects (or Flash cookies) to
+          collect and store information about your preferences and how you
+          interact with our websites and mobile applications. These technologies
+          help us to recognize you, customize or personalize your shopping
+          experience, store items in your online shopping cart between visits,
+          and analyze the use of our services and solutions to make them more
+          useful to you. These technologies also allow us to aggregate
+          demographic and statistical data and compilations of information,
+          which may or may not include personal information, and provide this
+          information to our service providers.
+        </p>
+        <p>
+          We and/or a service provider may also link this information with other
+          personal information about you and may use this linked information to
+          allow third parties to serve ads on our websites or mobile
+          applications or to serve our ads to you on a third party’s website or
+          mobile application. If you “click through” to or otherwise visit
+          third-party websites or mobile applications, they may also collect
+          information about you and your visit. This Privacy Policy does not
+          apply to, and we are not responsible for, the practices of other third
+          parties that collect your personal information. We encourage you to
+          check the privacy policies of other third parties whose information
+          you find on our website to learn about their practices with respect to
+          the collection, use, disclosure, and protection of your personal
+          information.
+        </p>
+
+        <p>
+          Most Internet browsers allow you to remove or manage cookie functions
+          and adjust your privacy and security preferences. For information on
+          how to do this, access the “help” menu on your Internet browser. Flash
+          cookies operate differently than browser cookies and cookie management
+          tools available in a web browser may not remove Flash cookies. To
+          learn how you can manage your Flash cookie settings, please visit
+          Adobe’s website. Please note, however, that disabling our cookies may
+          result in your inability to take full advantage of all of the features
+          of our services.
+        </p>
+
+        <p>
+          We also offer AdChoices on some of our websites which provides you
+          with the ability to opt out of certain third party services that may
+          involve tracking. For more information, click on the AdChoices link
+          listed under “About This Site” on our websites that offer this
+          service.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Privacy;
