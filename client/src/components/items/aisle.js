3ee81252ea6b600c21c310c9322a13a3e9567525
HongbinZheng <36983590+HongbinZheng@users.noreply.github.com>
2018-11-17 00:13:40 -0800

    animation for aisle

--- client/src/components/items/aisle.js
+++ client/src/components/items/aisle.js
@@ -2,7 +2,32 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
 
-import Cart from '../shopping cart/Cart';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Grow from '@material-ui/core/Zoom';
+
+const styles = theme => ({
+    root: {
+        height: 180,
+      },
+      wrapper: {
+        width: 100 + theme.spacing.unit * 2,
+      },
+    paper: {
+      margin: theme.spacing.unit,
+    },
+    svg: {
+      width: 100,
+      height: 100,
+    },
+    polygon: {
+      fill: theme.palette.common.white,
+      stroke: theme.palette.divider,
+      strokeWidth: 1,
+    },
+  });
+  
 
 class Aisle extends Component {
    constructor(){
@@ -60,47 +85,58 @@ handleAddtoCart(stuff){
 }
 
 render() {
+    var count = 0
   return (
-      this.state  ?
-      <div className = "container-fluid" style={{minHeight:window.innerHeight-245, marginTop:'4%'}}>
-      <div style={{margin:'auto'}}>
-       <h1>
-       {this.state.item.map(items =>
+      this.state ?
+          <div className="container-fluid" style={{ minHeight: window.innerHeight - 245, marginTop: '4%' }}>
+              <div style={{ margin: 'auto' }}>
+                  <h1>
+                      {this.state.item.map(items => {
+                          count = count + 200;
+                          return(
+                          <Grow
+                              in={true}
+                              style={{ transformOrigin: '0 0 0' }}
+                              {...({ timeout: count + 1000 })}
+                          >
+                              <div key={items.itemNo} className='rounded' style={{ margin: '10px', border: '1px solid #C2C2C2', display: "inline-block" }}>
+                                  <div className='card' style={{ width: '20rem', height: '28rem' }} >
+                                      <Link to={`/aisle/${items.aisle}/${items.name}`}>
+                                          <img className='card-img-top' style={{ width: '318px', height: '212.28px' }} src={`/images/aisle/${items.name}.png`} alt='Card cap'></img>
+                                      </Link>
+                                      <div className='card-body'>
+                                          <Link to={`/aisle/${items.aisle}/${items.name}`}>
+                                              <h2 className='card-title' style={{ textAlign: 'center', height: '50px', color: '#708090', marginTop: '20px' }}>{items.name}</h2>
+                                          </Link>
+                                      </div>
 
-       <div key={items.itemNo} className='rounded' style={{margin:'10px',border:'1px solid #C2C2C2',display:"inline-block"}}>
-           <div className='card' style={{width:'20rem', height:'28rem'}} >
-               <Link to={`/aisle/${items.aisle}/${items.name}`}>
-               <img className='card-img-top' style={{width:'318px',height:'212.28px'}} src={`/images/aisle/${items.name}.png`} alt='Card cap'></img>
-               </Link>
-               <div className='card-body'>
-               <Link to={`/aisle/${items.aisle}/${items.name}`}>
-               <h2 className='card-title' style={{textAlign:'center', height: '50px', color:'#708090', marginTop:'20px'}}>{items.name}</h2>
-               </Link>
-               </div>
-                
-               {items.discount !== 1 ?
-               <div style = {{position: 'relative', textAlign:'center', marginBottom:'23px'}}>
-               <p className='card-text' style={{textAlign:'center',textDecorationLine:'line-through', fontSize: 25, color:'grey', display:'inline'}}>${items.price.toFixed(2)}</p>
-               <p className='card-text' style={{textAlign:'center',color:'red',fontStyle:'bold', display:'inline', fontSize: 30}}>${(items.price * items.discount).toFixed(2)}</p>
-               </div>
-               :
-                <div style={{marginBottom:'24px'}}>
-               <p className='card-text' style={{textAlign:'center', fontSize: 30, fontStyle:'bold'}}>${(items.price * items.discount).toFixed(2)}</p>
-                </div>
-               }
-               
-               <button onClick={()=>this.handleAddtoCart(items)} className='btn btn-info' style={{position:'relative', marginBottom:'0px'}} >Add to cart  <i className="fas fa-cart-plus"></i></button>
-           </div>
-       </div>
-       )
-       }
-       </h1>
-       </div>
-      </div> : <div>
-          This is nothing page
+                                      {items.discount !== 1 ?
+                                          <div style={{ position: 'relative', textAlign: 'center', marginBottom: '23px' }}>
+                                              <p className='card-text' style={{ textAlign: 'center', textDecorationLine: 'line-through', fontSize: 25, color: 'grey', display: 'inline' }}>${items.price.toFixed(2)}</p>
+                                              <p className='card-text' style={{ textAlign: 'center', color: 'red', fontStyle: 'bold', display: 'inline', fontSize: 30 }}>${(items.price * items.discount).toFixed(2)}</p>
+                                          </div>
+                                          :
+                                          <div style={{ marginBottom: '24px' }}>
+                                              <p className='card-text' style={{ textAlign: 'center', fontSize: 30, fontStyle: 'bold' }}>${(items.price * items.discount).toFixed(2)}</p>
+                                          </div>
+                                      }
+                                      <button onClick={() => this.handleAddtoCart(items)} className='btn btn-info' style={{ position: 'relative', marginBottom: '0px' }} >Add to cart  <i className="fas fa-cart-plus"></i></button>
+                                  </div>
+                              </div>
+                          </Grow>
+                      )})
+                      }
+                  </h1>
+              </div>
+          </div> : <div>
+              This is nothing page
       </div>
   );
 }
 }
 
-export default Aisle;
+Aisle.propTypes = {
+    classes: PropTypes.object.isRequired,
+  };
+  
+  export default withStyles(styles)(Aisle);
