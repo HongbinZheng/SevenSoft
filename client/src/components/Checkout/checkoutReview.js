38a1b75e0b939999f0a5f2d25e038f819c0c167c
Kevin Prakasa <kevinprakasa15@gmail.com>
2018-11-19 15:58:21 -0800

    checkout form updated

--- client/src/components/Checkout/checkoutReview.js
+++ client/src/components/Checkout/checkoutReview.js
@@ -1,105 +1,141 @@
-import React, { Component } from 'react';
-import { Redirect, Link } from 'react-router-dom'
+import React, { Component } from "react";
+import { Redirect, Link } from "react-router-dom";
 
-import {firebaseDB} from '../../firebase';
-import Authserver from'../authserver'
-import jwt from'jsonwebtoken';
-
+// import {firebaseDB} from '../../firebase';
+import Authserver from "../authserver";
+// import jwt from'jsonwebtoken';
 
 class CheckoutReview extends Component {
-    constructor(props) {
-        super(props)
-        this.state={
-            redirect:false
-        }
-        if(localStorage.getItem('cart') != null) {
-            var cartString = localStorage.getItem('cart')
-            var cart = JSON.parse(cartString)
-            this.state ={cartItems: this.getItemsFromCart(cart), totalPrice: 0,qty:0,redirect:false}
-          }
-          this.getTotalPrice(this.state.cartItems);
-          this.Auth = new Authserver();
+  constructor(props) {
+    super(props);
+    this.state = {
+      redirect: false
+    };
+    if (localStorage.getItem("cart") != null) {
+      var cartString = localStorage.getItem("cart");
+      var cart = JSON.parse(cartString);
+      this.state = {
+        cartItems: this.getItemsFromCart(cart),
+        totalPrice: 0,
+        qty: 0,
+        redirect: false
+      };
     }
+    this.getTotalPrice(this.state.cartItems);
+    this.Auth = new Authserver();
+  }
 
-    getItemsFromCart = (cart) => {
-        var cartItems = []
-        for(var itemID in cart) {
-          cartItems.push(cart[itemID])
-        }
-        return cartItems
-      }
-
-      handleCheckOut(items){
-        // if(this.Auth.loggedIn()){
-        //     var SERECT = "superserect"
-        //     const token = localStorage.getItem('id_token')
-        //     var decoded = jwt.verify(token, SERECT);
-        //     items.forEach(item=>{item.myRate = 0});
-        //     console.log(items);
-        //     firebaseDB.ref(`/orders/${decoded}`).push(items);
-        // }
-       // this.props.history.push('/Checkout')
-       this.setState({redirect:true})
-      }
-
-      getTotalPrice(items) {
-        var tPrice = 0
-        items.forEach((item) => {
-            tPrice += item.price * item.discount * item.quantityInCart
-        })
-        return tPrice
+  getItemsFromCart = cart => {
+    var cartItems = [];
+    for (var itemID in cart) {
+      cartItems.push(cart[itemID]);
     }
+    return cartItems;
+  };
 
+  handleCheckOut(items) {
+    // if(this.Auth.loggedIn()){
+    //     var SERECT = "superserect"
+    //     const token = localStorage.getItem('id_token')
+    //     var decoded = jwt.verify(token, SERECT);
+    //     items.forEach(item=>{item.myRate = 0});
+    //     console.log(items);
+    //     firebaseDB.ref(`/orders/${decoded}`).push(items);
+    // }
+    // this.props.history.push('/Checkout')
+    this.setState({ redirect: true });
+  }
 
-    render() {
-        const { redirect, carItems } = this.state
+  getTotalPrice(items) {
+    var tPrice = 0;
+    items.forEach(item => {
+      tPrice += item.price * item.discount * item.quantityInCart;
+    });
+    return tPrice;
+  }
 
-        if (redirect){
-            var obj = {...this.state.cartItems}
-        return (<Redirect to={{
-            pathname: '/checkout',
+  render() {
+    const { redirect } = this.state;
+
+    if (redirect) {
+      var obj = { ...this.state.cartItems };
+      return (
+        <Redirect
+          to={{
+            pathname: "/checkout",
             state: { referrer: obj }
-        }} />)
+          }}
+        />
+      );
     }
-        return (
-            <div className="container" style={{marginTop:"50px",marginBottom:"300px",width:"750px",textAlign:"center"}}>
-                <div className="row">
-                      <div className="col">
-                        Item Name
-                      </div>
-                      <div className="col">
-                        Qty
-                      </div>
-                      <div className="col">
-                        Price
-                      </div>
-                    </div>
-                {this.state.cartItems.map((item,index) => {
-                    return(
-                    <div className="container" key={item.itemid} style={{border:"1px solid #000000",width:"700px",height:"150px",alignItems: 'center',backgroundColor:"white"}}>
-                            <div className="row" style={{marginTop:"30px"}}>
-                            <div className="col">
-                                <img className="card-img-top" style={{width:'40%',height:'40%'}} src={`/images/aisle/${item.name}.png`} alt='Card cap'/>
-                                    <h5>{item.name}</h5>
-                                    </div>
-                                    <div className="col">
-                                    <h3>
-                                    {item.quantityInCart}</h3>
-                                    </div>
-                                    <div className="col">
-                                    <h3 className="card-text">${(item.price * item.discount*item.quantityInCart).toFixed(2)}</h3>
-                                    </div>
-                            </div>
-                  </div>
-                    )
-                })}
-                <div className="col" style={{marginTop:"50px"}}>
-                <h2 className="card-text text-right" >Subtotal: ${this.getTotalPrice(this.state.cartItems).toFixed(2)}</h2>
-                <button className="check out button" onClick={()=>this.handleCheckOut(this.state.cartItems)}> Continue to Checkout</button>
+    return (
+      <div
+        className="container"
+        style={{
+          marginTop: "50px",
+          marginBottom: "300px",
+          width: "750px",
+          textAlign: "center"
+        }}
+      >
+        <div className="row">
+          <div className="col">Item Name</div>
+          <div className="col">Qty</div>
+          <div className="col">Price</div>
+        </div>
+        {this.state.cartItems.map((item, index) => {
+          return (
+            <div
+              className="container"
+              key={item.itemid}
+              style={{
+                border: "1px solid #000000",
+                width: "700px",
+                height: "150px",
+                alignItems: "center",
+                backgroundColor: "white"
+              }}
+            >
+              <div className="row" style={{ marginTop: "30px" }}>
+                <div className="col">
+                  <img
+                    className="card-img-top"
+                    style={{ width: "40%", height: "40%" }}
+                    src={`/images/aisle/${item.name}.png`}
+                    alt="Card cap"
+                  />
+                  <h5>{item.name}</h5>
                 </div>
+                <div className="col">
+                  <h3>{item.quantityInCart}</h3>
+                </div>
+                <div className="col">
+                  <h3 className="card-text">
+                    $
+                    {(item.price * item.discount * item.quantityInCart).toFixed(
+                      2
+                    )}
+                  </h3>
+                </div>
+              </div>
             </div>
-        );
-    }
+          );
+        })}
+        <div className="col" style={{ marginTop: "50px" }}>
+          <h2 className="card-text text-right">
+            Subtotal: ${this.getTotalPrice(this.state.cartItems).toFixed(2)}
+          </h2>
+          <button
+            className="check out button"
+            onClick={() => this.handleCheckOut(this.state.cartItems)}
+          >
+            {" "}
+            Continue to Checkout
+          </button>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default CheckoutReview;
