39b95bbf2d98915702a927859985b035f3f5e95c
it-jinshan <it.jinshan@gmail.com>
2018-11-20 16:16:49 -0800

    finished aisle cover pics

--- client/src/components/items/allAisle.js
+++ client/src/components/items/allAisle.js
@@ -8,74 +8,67 @@ import Grow from '@material-ui/core/Grow';
 
 window.__MUI_USE_NEXT_TYPOGRAPHY_VARIANTS__ = true;
 const styles = theme => ({
-  root: {
-    height: 180,
-  },
-  container: {
-    display: 'flex',
-    textAlign:'center',
-  },
-  paper: {
-    margin: theme.spacing.unit,
-  },
-  typography: {
-    useNextVariants: true,
-  },
+ root: {
+   height: 180,
+ },
+ container: {
+   display: 'flex',
+   textAlign:'center',
+ },
+ paper: {
+   margin: theme.spacing.unit,
+ },
+ typography: {
+   useNextVariants: true,
+ },
 });
 
 const aisle =['Beverages','Dairy','Meats','Produce','Snacks']
 
 class AllAisle extends React.Component {
-  state = {
-    checked: true,
-  };
+ state = {
+   checked: true,
+ };
 
-  handleChange = () => {
-    this.setState(state => ({ checked: !state.checked }));
-  };
+ handleChange = () => {
+   this.setState(state => ({ checked: !state.checked }));
+ };
 
-  render() {
-    const { classes } = this.props;
-    const { checked } = this.state;
-    var count = 0;
-    return (
-<<<<<<< HEAD
-      <div className={classes.root} >
-      <div className="container-fluid" style={{minHeight:window.innerHeight-245, marginTop:'54px'}}>
-        <div className={classes.container}>
-          {aisle.map(items => {
-=======
-      <div className = "container-fluid" style={{minHeight:window.innerHeight-245, marginTop:'54px', fontFamily:'Lucida Handwriting'}}>
-        {aisle.map(items => {
->>>>>>> 73addfef66cea2ad3e0be8d118dee838a5aee701
-            count = count + 200;
-                    return (
-                        <Grow
-                            in={checked}
-                            style={{ transformOrigin: "0 0 0" }}
-                            {...(checked ? { timeout: 1000 + count } : {})}
-                        >
-                                <div key={items.key} className='rounded' style={{border: '1px solid #C2C2C2', display: "inline-block",margin:'10px' }}>
-                                <Link to={`/aisle/${items}`}>
-                                    <div className='card' style={{ width: '20rem', height: '28rem' }} >
-                                            <img className='card-img-top' style={{ width: '318px', height: '212.28px' }} src={`/images/aisle/${items}.png`} alt='Card cap'></img>
-                                            <div className='card-body'>
-                                                <h2 className='card-title' style={{ textAlign: 'center', height: '50px', color: '#708090', marginTop: '20px' }}>{items}</h2>
-                                                </div>
-                                        </div>
-                                </Link>
-                                </div>
+ render() {
+   const { classes } = this.props;
+   const { checked } = this.state;
+   var count = 0;
+   return (
+     <div className = "container-fluid" style={{minHeight:window.innerHeight-245, marginTop:'4%', fontFamily:'Lucida Handwriting'}}>
+       {aisle.map(items => {
+           count = count + 200;
+                   return (
+                       <Grow
+                           in={checked}
+                           style={{ transformOrigin: "0 0 0" }}
+                           {...(checked ? { timeout: 1000 + count } : {})}
+                       >
+                               <div key={items.key} className='rounded' style={{border: '1px solid #C2C2C2', display: "inline-block",margin:'10px' }}>
+                               <Link to={`/aisle/${items}`}>
+                                   <div className='card' style={{ width: '20rem', height: '28rem' }} >
+                                           <img className='card-img-top' style={{ width: '318px', height: '212.28px' }} src={`/images/aisle/${items}.png`} alt='Card cap'></img>
+                                           <div className='card-body'>
+                                               <h2 className='card-title' style={{ textAlign: 'center', height: '50px', color: '#708090', marginTop: '20px' }}>{items}</h2>
+                                               </div>
+                                       </div>
+                               </Link>
+                               </div>
 
-                        </Grow>
-                    );
-          })}
-        </div>
-      );
-  }
+                       </Grow>
+                   );
+         })}
+       </div>
+     );
+ }
 }
 
 AllAisle.propTypes = {
-  classes: PropTypes.object.isRequired,
+ classes: PropTypes.object.isRequired,
 };
 
 export default withStyles(styles)(AllAisle);
