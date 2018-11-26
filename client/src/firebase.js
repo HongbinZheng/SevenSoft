d8fd8be2952c9a887ee452c25e56053f0ec56ff0
HongbinZheng <36983590+HongbinZheng@users.noreply.github.com>
2018-11-26 00:13:39 -0800

    Update firebase.js

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -1,12 +1,12 @@
 import * as firebase from 'firebase';
 
 const config = {
-    apiKey: "AIzaSyCEK08XguV4CVa4balQ05DD-zj1L8I57QY",
-    authDomain: "minisafeway-ac266.firebaseapp.com",
-    databaseURL: "https://minisafeway-ac266.firebaseio.com",
-    projectId: "minisafeway-ac266",
-    storageBucket: "minisafeway-ac266.appspot.com",
-    messagingSenderId: "1031101506009"
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: ""
 };
 
 firebase.initializeApp(config);
