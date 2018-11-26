18fc256f441d2f4b11c37aec75c8b59129dd767a
HongbinZheng <36983590+HongbinZheng@users.noreply.github.com>
2018-11-26 00:14:50 -0800

    Update server.js

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ var nodemailer = require('nodemailer');
 var connect = mysql.createConnection({
     host: 'lancedb.cjgoraxv1j8k.us-west-1.rds.amazonaws.com',
     user: 'lanceypants',
-    password: 'panceylants',
+    password: '',
     port:'3303',
     database: 'minisafeway'
 });
@@ -30,12 +30,12 @@ global.db = connect;
 
 ////////////////Firebase config///////////////////////////////
 const config = {
-  apiKey: "AIzaSyCEK08XguV4CVa4balQ05DD-zj1L8I57QY",
-  authDomain: "minisafeway-ac266.firebaseapp.com",
-  databaseURL: "https://minisafeway-ac266.firebaseio.com",
-  projectId: "minisafeway-ac266",
-  storageBucket: "minisafeway-ac266.appspot.com",
-  messagingSenderId: "1031101506009"
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: ""
 };
 
 firebase.initializeApp(config);
